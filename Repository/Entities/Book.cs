61189671e0b2285057dd33c91580a6ef5e4bf0ee
doctorboo <doctor.boo@outlook.com>
2015-04-02 23:11:21 +0200

    reporsitory

---
+++ Repository/Entities/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+	public class Book
+	{
+		[Required]
+		public string Id { get; set; }
+		[Required]
+		[StringLength(255)]
+		public string Author { get; set; }
+		[Required]
+		[StringLength(255)]
+		public string Title { get; set; }
+		[Required]
+		public Genre Category{ get; set; }
+		public DateTime? PublishDate { get; set; }
+		[Required]
+		public decimal Price { get; set; }
+		public string Description { get; set; }
+	}
+}
