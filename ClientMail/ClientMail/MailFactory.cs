e88672df517f0f8c31e21b0480d0284ee5d56b01
DoctorBoo <doctor.boo@outlook.com>
2015-05-14 12:28:04 +0200

    office

---
+++ ClientMail/ClientMail/MailFactory.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.ComponentModel;
+using System.Net;
+using System.Collections.Concurrent;
+using System.Threading;
+using Microsoft.Office.Interop.Outlook;
+
+namespace ClientMail
+{
+    public class OfficeFactory
+    {
+        object locker = new object();
+        public OfficeFactory()
+        {
+
+        }
+        public static bool mailSent = false;
+        public static ConcurrentDictionary<string, string> _tokens;
+        private void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            lock (locker)
+            {
+                // Get the unique identifier for this asynchronous operation.
+                String token = (string)e.UserState;
+
+                if (e.Cancelled)
+                {
+                    Console.WriteLine("[{0}] Send canceled.", token);
+                }
+                if (e.Error != null)
+                {
+                    Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Message sent.");
+                }
+                mailSent = true;
+                _tokens.TryAdd(token, token);
+                Monitor.PulseAll(locker);
+            }
+            
+        }
+        public void Send(string server, int port, string userName, string password, object token)
+        {
+            lock (locker)
+            {
+                if (_tokens == null) _tokens = new ConcurrentDictionary<string, string>();
+
+                // Command line argument must the the SMTP host.            
+                SmtpClient client = new SmtpClient(server, port);
+                // Credentials are necessary if the server requires the client  
+                // to authenticate before it will send e-mail on the client's behalf.            
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential(userName, password);
+                // Specify the e-mail sender. 
+                // Create a mailing address that includes a UTF8 character 
+                // in the display name.
+                MailAddress from = new MailAddress("jane@contoso.com",
+                   "Jane " + (char)0xD8 + " Clayton",
+                System.Text.Encoding.UTF8);
+                // Set destinations for the e-mail message.
+                MailAddress to = new MailAddress("sneakyboo@gmail.com");
+                // Specify the message content.
+                using (MailMessage message = new MailMessage(from, to))
+                {
+                    message.Body = "This is a test e-mail message sent by an application. ";
+                    // Include some non-ASCII characters in body and subject. 
+                    string someArrows = new string(new char[] { '\u2190', '\u2191', '\u2192', '\u2193' });
+                    message.Body += Environment.NewLine + someArrows;
+                    message.BodyEncoding = System.Text.Encoding.UTF8;
+                    message.Subject = "test message 1" + someArrows;
+                    message.SubjectEncoding = System.Text.Encoding.UTF8;
+                    // Set the method that is called back when the send operation ends.
+
+                    client.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
+
+                    // The userState can be any object that allows your callback  
+                    // method to identify this send operation. 
+                    // For this example, the userToken is a string constant. 
+                    string userState = string.Format("{0}", token.ToString());
+                    client.SendAsync(message, userState);
+                    Monitor.Wait(locker);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ClientMail/MailTest/SendTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+using System.Threading;
+using ClientMail;
+using Microsoft.Office.Interop.Outlook;
+
+namespace TestRepo
+{
+    /// <summary>
+    /// Summary description for Mail
+    /// </summary>
+    [TestClass]
+    public class Mail
+    {
+        object locker = new object();
+        public Mail()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+        private static string token;
+        private static bool mailSent;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+
+        }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMail()
+        {
+            lock (locker)
+            {
+                String expectedToken = Guid.NewGuid().ToString();
+
+                OfficeFactory factory = new OfficeFactory();
+                factory.Send("smtp.gmail.com", 587, "sygnion", "sygnion01!", expectedToken);
+                //Monitor.Wait(locker);
+
+                string actual;
+                OfficeFactory._tokens.TryGetValue(expectedToken, out actual);
+                Assert.AreEqual(expectedToken, actual);
+            }
+        }
+        public void TestAppointment()
+        {
+            OfficeFactory office = new OfficeFactory();
+            
+        }
+    }
+}
