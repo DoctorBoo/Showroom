15ba76004590ea32eb81c0d7b99e49938717857c
DoctorBoo <doctor.boo@outlook.com>
2015-05-15 16:56:10 +0200

    stresstest

--- ClientMail/MailTest/SendTest.cs
+++ ClientMail/MailTest/SendTest.cs
@@ -3,7 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.ComponentModel;
 using System.Threading;
 using ClientMail;
-using Microsoft.Office.Interop.Outlook;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace TestRepo
 {
@@ -24,6 +25,8 @@ namespace TestRepo
         private TestContext testContextInstance;
         private static string token;
         private static bool mailSent;
+        private readonly int length = 25;
+
 
         /// <summary>
         ///Gets or sets the test context which provides
@@ -73,8 +76,8 @@ namespace TestRepo
             {
                 String expectedToken = Guid.NewGuid().ToString();
 
-                OfficeFactory factory = new OfficeFactory();
-                factory.Send("smtp.gmail.com", 587, "sygnion", "sygnion01!", expectedToken);
+                OfficeFactory factory = new OfficeFactory("smtp.gmail.com", 587, "sygnion2", "sygnion01!");
+                factory.SendCalendarEvent("dmodiwirijo@hotmail.com", " Dwight Modiwirijo | YMultego", null, new DateTime(2015, 7, 7), expectedToken);
                 //Monitor.Wait(locker);
 
                 string actual;
@@ -82,10 +85,80 @@ namespace TestRepo
                 Assert.AreEqual(expectedToken, actual);
             }
         }
-        public void TestAppointment()
+        [TestMethod]
+        public void StressTest()
         {
-            OfficeFactory office = new OfficeFactory();
-            
+            Action[] jobQueue = new Action[4]; 
+            ConcurrentQueue<Exception> errors = new ConcurrentQueue<Exception>();
+            for (int i = 0; i < 4; i++)
+            {
+                jobQueue[i] = () =>
+                {
+                    using (OfficeFactory factory = new OfficeFactory("smtp.gmail.com", 587, "sygnion2", "sygnion01!"))
+                    {
+                        ConcurrentBag<Task> tasks = new ConcurrentBag<Task>();
+                        ConcurrentBag<Action<string, DateTime, string>> bag = new ConcurrentBag<Action<string, DateTime, string>>();
+
+                        Random rnd = new Random();
+                        Action[] actionList = new Action[length];
+
+                        //Create jobs
+                        for (int j = 0; j < length; j++)
+                        {
+                            Action action = () =>
+                            {
+                                lock (locker)
+                                {
+                                    {
+                                        try
+                                        {
+                                            String expectedToken = Guid.NewGuid().ToString();
+                                            int month = rnd.Next(1, 12);
+                                            int day = month == 2 ? rnd.Next(1, 28) : rnd.Next(1, 30);
+                                            factory.SendCalendarEvent("dmodiwirijo@hotmail.com", "STRESSTEST", null, new DateTime(2015, month, day), expectedToken);
+                                            string actual;
+                                            OfficeFactory._tokens.TryGetValue(expectedToken, out actual);
+                                            Assert.AreEqual(expectedToken, actual);
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            errors.Enqueue(ex);
+                                        }
+                                    }
+                                }
+                            };
+                            actionList[j] = action;
+                        }
+
+                        //Execute jobs parallel
+                        ParallelLoopResult result = Parallel.For(0, length, ctr =>
+                        {
+                            var task = Task.Factory.StartNew(actionList[ctr]);
+                            tasks.Add(task);
+                        });
+
+                        //Wait on all finished
+                        Task.WaitAll(tasks.ToArray());
+                        foreach (var failure in OfficeFactory._failure)
+                        {
+                            Console.WriteLine("[{0}]\r\n{1}", failure.Key, failure.Value);
+                        }
+
+                        foreach (var error in errors)
+                        {
+                            Console.WriteLine("[{0}]\r\n{1}", error.Message, error.InnerException != null ? error.InnerException.Message : null);
+                        }
+                    }
+                };
+            };
+
+            ConcurrentBag<Task> tasks2 = new ConcurrentBag<Task>();
+            foreach (var job in jobQueue)
+            {
+                var task = Task.Factory.StartNew(job);
+                tasks2.Add(task);
+            }
+            Task.WaitAll(tasks2.ToArray());
         }
     }
 }
