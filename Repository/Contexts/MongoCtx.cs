d5fbe7a55144766b4ac75e352b2a505799f304c2
DoctorBoo <doctor.boo@outlook.com>
2015-05-31 18:13:07 +0200

    connectionstrng mongodb OK

---
+++ Repository/Contexts/MongoCtx.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Contexts
+{
+	public class MongoCtx
+	{
+		protected static IMongoClient _client;
+		protected static IMongoDatabase _database;
+
+		public IMongoCollection<BsonDocument> GetData(string catalogDb, string document)
+		{
+			var connectionString = "mongodb://localhost:27017/restautants?connect=automatic";
+
+			//take database name from connection string
+			var _databaseName = MongoUrl.Create(connectionString).DatabaseName;
+			_client = new MongoClient(connectionString);			
+			//var _server = _client.GetServer() as MongoClient;
+						
+			_database = _client.GetDatabase(catalogDb);
+			var collection = _database.GetCollection<BsonDocument>(document);
+			//await collection.InsertOneAsync(document);
+
+			return collection;
+		}
+	}
+}
--- Repository/Helpers/Extensions.cs
+++ Repository/Helpers/Extensions.cs
@@ -5,11 +5,63 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Principal;
 using System.Security.Claims;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections;
 
 namespace Repository.Helpers
 {
 	public static class Extensions
 	{
+		public static async Task<IList> AddBsonDocumentsAsync(this IList list, IMongoCollection<BsonDocument> bsonDocs)
+		{
+			var filter = new BsonDocument();
 
+			try
+			{
+			var count = 0;
+			using (var cursor = await bsonDocs.FindAsync(filter))
+			{
+				while (await cursor.MoveNextAsync())
+				{
+					var batch = cursor.Current;
+					foreach (var document in batch)
+					{
+						// process document
+						list.Add(document);
+						count++;
+					}
+				}
+			}
+			}
+			catch (Exception)
+			{				
+				throw;
+			}
+			return list;
+		}
+
+		/// <summary>
+		/// Counts async the items in collection.
+		/// </summary>
+		/// <param name="list"></param>
+		/// <returns></returns>
+		public static long Count(this IMongoCollection<BsonDocument> list)
+		{
+			return DoCount(list);
+		}
+		public static long DoCount(IMongoCollection<BsonDocument> list)
+		{
+			var result = GetDocumentCount(list, "{}");
+			result.Wait();
+
+			long count = result.Result;
+			return count;
+		}
+		private async static Task<long> GetDocumentCount(IMongoCollection<BsonDocument> list, FilterDefinition<BsonDocument> filter)
+		{
+			long count = await list.CountAsync(filter);
+			return count;
+		}
 	}
 }
