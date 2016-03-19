1d97092eea24c5e57f9464e98cb56a416fb6ffc8
DoctorBoo <doctor.boo@outlook.com>
2016-03-19 12:31:20 +0100

    layers added

---
+++ Repository/Helpers/Repository.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Helpers
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity>, IDisposable
+        where TEntity : class
+        where TContext : DbContext
+    {
+        protected TContext Context;
+
+        public Repository(TContext dbContext)
+        {
+            Context = dbContext as TContext;
+        }
+
+        public virtual TEntity Create()
+        {
+            return Context.Set<TEntity>().Create();
+        }
+
+        public virtual TEntity Create(TEntity entity)
+        {
+            return Context.Set<TEntity>().Add(entity);
+        }
+        public virtual TEntity Update(TEntity entity)
+        {
+            try
+            {
+                Context.Entry(entity).State = EntityState.Detached;
+                Context.Set<TEntity>().Attach(entity);
+                Context.Entry(entity).State = EntityState.Modified;
+
+                bool succeeded = 0 < Context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            return entity;
+        }
+
+        public virtual void Remove(decimal id)
+        {
+            var item = Context.Set<TEntity>().Find(id);
+            Context.Set<TEntity>().Remove(item);
+            bool succeeded = 0 < Context.SaveChanges();
+        }
+
+        public virtual void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+            bool succeeded = 0 < Context.SaveChanges();
+        }
+
+        public virtual void Delete(Expression<Func<TEntity, bool>> where)
+        {
+            var objects = Context.Set<TEntity>().Where(where).AsEnumerable();
+            foreach (var item in objects)
+            {
+                Context.Set<TEntity>().Remove(item);
+            }
+        }
+
+        public virtual TEntity FindById(decimal id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+
+        public virtual TEntity FindOne(Expression<Func<TEntity, bool>> where = null)
+        {
+            return FindAll(where).FirstOrDefault();
+        }
+
+        public IQueryable<T> Set<T>() where T : class
+        {
+            return Context.Set<T>();
+        }
+
+        public virtual IQueryable<TEntity> FindAll(Expression<Func<TEntity, bool>> where = null)
+        {
+            return null != where ? Context.Set<TEntity>().Where(where) : Context.Set<TEntity>();
+        }
+        public virtual IList<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+        public virtual bool SaveChanges()
+        {
+            bool succeeded = 0 < Context.SaveChanges();
+            return succeeded;
+        }
+        public virtual async Task<int> SaveChangesAsync()
+        {
+            return await Context.SaveChangesAsync();
+        }
+        /// <summary>
+        /// Releases all resources used by the Entities
+        /// </summary>
+        public void Dispose()
+        {
+            if (null != Context)
+            {
+                Context.Dispose();
+            }
+        }
+    }
+}
